1d82c6f2d487936734bb202e8ef7988f1b923a11
Andrii Dmytrenko <andrii.dmytrenko@deliveroo.co.uk>
2018-04-06 17:29:47 +0100

    Switch to using tokio async IO

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
 extern crate byteorder;
+extern crate chrono;
+extern crate tokio;
+extern crate tokio_dns;
 
-use byteorder::{BigEndian, ReadBytesExt};
 use std::env;
 use std::io::Cursor;
-use std::net::UdpSocket;
-use std::time::Duration;
+use std::net::SocketAddr;
+use std::time::{Duration, Instant};
+
+use byteorder::{BigEndian, ReadBytesExt};
+use chrono::{TimeZone, Utc};
+use tokio::io;
+use tokio::net::UdpSocket;
+use tokio::prelude::future::*;
+use tokio::prelude::*;
 
 // From page 18 of https://www.ietf.org/rfc/rfc5905.txt:
 //
@@ -42,74 +51,104 @@ const TRANSMIT_TS_OFFSET: u64 = 40;
 // where: LI = 0; VN, Mode = 3
 // altogether: 000011011 -> 1b (base 16)
 // padding the rest with zeros
-const REQUEST_HEADER: &'static [u8] = &[
-    0x1b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+const REQUEST_HEADER: &[u8] = &[
+    0x1b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
 ];
 
 // Unix epoch is 1970-01-01T00:00:00Z
 // NTP epoch is 1900-01-01T00:00:00Z
 // with 17 leap years in between
 // (70 * 365 + 17) * 86400 = 2208988800
-const UNIX_EPOCH_OFFSET: u32 = 2208988800;
+const UNIX_EPOCH_OFFSET: u32 = 2_208_988_800;
 
-const BIND_ADDRESS: &'static str = "0.0.0.0:0";
+const BIND_ADDRESS: &str = "0.0.0.0:0";
 
 const BUFFER_SIZE: usize = 1024;
 
-const DEFAULT_NTP_PORT: u32 = 123;
-const DEFAULT_NTP_HOST: &'static str = "pool.ntp.org";
+const DEFAULT_NTP_PORT: u16 = 123;
+const DEFAULT_NTP_HOST: &str = "pool.ntp.org";
 
 const SOCKET_TIMEOUT: u64 = 2500;
 
-fn receive_timestamp(ntp_host: &str) -> u32 {
-    let mut buffer = vec![0u8; BUFFER_SIZE];
-    let socket = UdpSocket::bind(BIND_ADDRESS).expect("couldn't bind to address");
-    let ntp_address = format!("{}:{}", ntp_host, DEFAULT_NTP_PORT);
-    let socket_timeout = Some(Duration::from_millis(SOCKET_TIMEOUT));
+// Custom error type to wrap other errors
+#[derive(Debug)]
+pub enum NtpError {
+    IoError(io::Error),
+    AddrParseError(std::net::AddrParseError),
+}
 
-    socket.set_write_timeout(socket_timeout).expect("set_write_timeout call failed");
-    socket.set_read_timeout(socket_timeout).expect("set_read_timeout call failed");
-
-    socket.send_to(REQUEST_HEADER, ntp_address).expect("couldn't send data");
-    socket.recv_from(&mut buffer).expect("didn't receive data");
-
-    let mut reader = Cursor::new(&buffer);
-
+fn parse_timestamp(buf: &[u8]) -> Result<chrono::DateTime<Utc>, NtpError> {
+    let mut reader = Cursor::new(&buf);
     reader.set_position(TRANSMIT_TS_OFFSET);
-    reader.read_u32::<BigEndian>().unwrap() - UNIX_EPOCH_OFFSET
     // TODO: extract milliseconds
+    reader
+        .read_u32::<BigEndian>()
+        .map(|val| Utc.timestamp(i64::from(val - UNIX_EPOCH_OFFSET), 0))
+        .map_err(NtpError::IoError)
+}
+
+pub fn receive_timestamp(
+    ntp_host: &str,
+    ntp_port: u16,
+) -> impl Future<Item = chrono::DateTime<Utc>, Error = NtpError> {
+    let bind_address = result(BIND_ADDRESS.parse().map_err(NtpError::AddrParseError));
+
+    let socket = bind_address
+        .and_then(|bind_address| result(UdpSocket::bind(&bind_address).map_err(NtpError::IoError)));
+
+    // just take the first IP address
+    let sock_addr = tokio_dns::resolve::<&str>(ntp_host)
+        .map(move |ips| SocketAddr::new(ips[0], ntp_port))
+        .map_err(NtpError::IoError);
+
+    // join will wait for both futures to resolve and then yield two results in a tuple
+    socket
+        .join(sock_addr)
+        .and_then(|(socket, addr)| {
+            socket
+                .send_dgram(REQUEST_HEADER, &addr)
+                .map_err(NtpError::IoError)
+        })
+        .and_then(|(socket, _buf)| {
+            let buffer = vec![0u8; BUFFER_SIZE];
+            socket.recv_dgram(buffer).map_err(NtpError::IoError)
+        })
+        .and_then(|(_socket, buf, _len, _sock_addr)| result(parse_timestamp(&buf)))
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let ntp_host = ntp_host_from_args(&args);
 
-    println!("{}", receive_timestamp(ntp_host));
+    let when = Instant::now() + Duration::from_millis(SOCKET_TIMEOUT);
+    let timestamp_future = receive_timestamp(ntp_host, DEFAULT_NTP_PORT)
+        .deadline(when)
+        .map(|timestamp| {
+            println!("{}", timestamp);
+        })
+        .map_err(|e| {
+            println!("Error receiving timestamp: {:?}", e);
+        });
+    tokio::run(timestamp_future);
 }
 
-fn ntp_host_from_args(args: &Vec<String>) -> &str {
-    let mut ntp_host: &str = &DEFAULT_NTP_HOST;
-
+fn ntp_host_from_args(args: &[String]) -> &str {
     if args.len() >= 2 {
-        ntp_host = &args[1];
+        &args[1]
+    } else {
+        DEFAULT_NTP_HOST
     }
-
-    ntp_host
 }
 
 #[test]
 fn test() {
     use std::{thread, time};
 
-    let t1 = receive_timestamp("pool.ntp.org");
+    let t1 = receive_timestamp("pool.ntp.org", DEFAULT_NTP_PORT).wait().unwrap();
     thread::sleep(time::Duration::from_millis(1000));
-    let t2 = receive_timestamp("time.nist.gov");
+    let t2 = receive_timestamp("time.nist.gov", DEFAULT_NTP_PORT).wait().unwrap();
 
     assert!(t2 > t1);
-    assert_eq!(t2 - t1, 1);
 }
